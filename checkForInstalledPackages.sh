26f6bdadc1cd2e5fa9ad98eb1e0fb408da08fef7
ShriramExtras <108141268+ShriramExtras@users.noreply.github.com>
2022-08-08 14:45:46 +0530

    Update checkForInstalledPackages.sh

--- checkForInstalledPackages.sh
+++ checkForInstalledPackages.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-if [[ $(which feh) != "" && $(which streamer) != "" ]]; then 
-	echo All packages Installed Successfully
-else 
-	clear
-	echo "Error: Unable to install packages"
-	echo Please check your internet connection or try again later.
-	sleep 3
-	exit 
-fi
+if [[ $(which feh) != "" && $(which streamer) != "" ]]; then
+        echo All packages Installed Successfully;
+else
+        clear;
+        echo "Error: Unable to install packages";
+        echo Please check your internet connection or try again later.;
+        sleep 3;
+        exit;
+fi;
