d34881f57390bdaa64245d8e971a265d16bae814
ShriramExtras <108141268+ShriramExtras@users.noreply.github.com>
2022-08-08 17:49:41 +0530

    Add files via upload

--- runSendOtp.sh
+++ runSendOtp.sh
@@ -4,10 +4,10 @@ wget -q --spider http://google.com
 
 if [ $? -eq 0 ]; then
     
-    echo "Sending verification code Please wait..."
+    echo "Processing your request, plese wait..."
     ../.mail/sendOtp 
     clear
-    echo "Verification code mailed to $(cat ../.mail/.mail.txt) "
+    echo "We have send verification code on $(cat ../.mail/.mail.txt) "
  
 fi
 
