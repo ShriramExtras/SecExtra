7d3038fee0f6f609e666b6f906f4238952971ad4
Ayush Ahirrao <90558594+AyushAhirrao@users.noreply.github.com>
2022-11-13 20:38:39 +0530

    Update runSendMailScript.sh

--- runSendMailScript.sh
+++ runSendMailScript.sh
@@ -3,10 +3,10 @@
 wget -q --spider http://google.com
 
 if [ $? -eq 0 ]; then
-    ../.mail/sendMailOnline
+    python3 ../.mail/sendMailOnline
 else
-    sudo cp ../.mail/sendMailOffline /etc/network/if-up.d/;
-    sudo cp ../.mail/.mail.txt /etc/network/if-up.d/mail.txt;
-    sudo cp ../.temp/.image.jpeg /etc/network/if-up.d/image.jpeg;
-    sudo cp ../.temp/.time.txt /etc/network/if-up.d/time.txt;
+    cp ../.mail/sendMailOffline /home/$USER/.runMail/;
+    cp ../.mail/.mail.txt /home/$USER/.runMail/mail.txt;
+    cp ../.temp/.image.jpeg /home/$USER/.runMail/image.jpeg;
+    cp ../.temp/.time.txt /home/$USER/.runMail/time.txt;
 fi
