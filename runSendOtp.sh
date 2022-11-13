c18492a3194529464bf4bbf8f15c923c9391d471
Ayush Ahirrao <90558594+AyushAhirrao@users.noreply.github.com>
2022-11-13 20:43:18 +0530

    Update runSendOtp.sh

--- runSendOtp.sh
+++ runSendOtp.sh
@@ -5,7 +5,7 @@ wget -q --spider http://google.com
 if [ $? -eq 0 ]; then
     
     echo "Processing your request, plese wait..."
-    ../.mail/sendOtp 
+    python3 ../.mail/sendOtp 
     clear
     echo "We have sent a verification code to $(cat ../.mail/.mail.txt)"
  
